7c38ddea3f31a5761a16d40f87813591bc48fe4a
KamilKapron <kapron.kam@gmail.com>
2021-06-08 12:29:48 +0100

    Dokumentacja beta v0.1

--- Shape.h
+++ Shape.h
@@ -8,22 +8,51 @@
 #include "type.h"
 
 #include <cmath>
-
+/**
+* Klasa wirtualna reprezentuj¹ca figurê obiekt w programie.
+*/
 class Shape
 {
 public:
+	/**
+	* Konstruktor. Tworzy figure, trójk¹t lub czworok¹t.
+	* @param c kolor figury.
+	* @param t typ figury czy trójk¹t b¹dŸ czworok¹t.
+	*/
 	Shape(wxColour c, Type t) : color(c), type(t), start(nullptr), pos(nullptr) {}
+	/**
+	* Rysuje figure na ekran.
+	* @param panel miejsce gdzie wyrysowywana jest figura.
+	*/
 	virtual void Draw(wxAutoBufferedPaintDC &panel) = 0;
+	/**
+	* Obraca figure o zadany k¹t wzglêdem jej œrodka.
+	* @param radian k¹t w radianach o jaki obracana jest figura.
+	*/
 	virtual void Rotate(double radian) = 0;
+	/**
+	* Przesuwa figurê o dane wspó³rzêdne.
+	* @param x przesuniêcie x pikesli w lewo b¹dŸ w prawo.
+	* @param y przesuniêcie y pikesli w górê b¹dŸ w dó³.
+	*/
 	virtual void Move(int x, int y) = 0;
+	/**
+	* Zwraca punkty w których zawartê s¹ wierzcho³ki figury.
+	*/
 	virtual wxPoint* GetPoints() const = 0;
+	/**
+	* Ustawia aktualne po³o¿enie figury na po³o¿enie pocz¹tkowe.
+	*/
 	virtual void Reset() const = 0;
+	/**
+	* Destruktor.
+	*/
 	virtual ~Shape() {}
 protected:
-	wxPoint* start;
-	wxPoint* pos;
+	wxPoint* start; /**< wskaŸnik na tablice zawieraj¹c¹ wspó³rzêdne pocz¹tkowe wierzcho³ków figury. */
+	wxPoint* pos; /**< wskaŸnik na tablice zawieraj¹c¹ aktualne wspó³rzêdne wierzcho³ków figury. */
 public:
-	const Type type;
-	bool in_container = true;
-	wxColour color;
+	const Type type; /**< Zmienna przechowywuj¹ca informacje jakiego typu jest figura (czy czworok¹t, czy trójk¹t). */
+	bool in_container = true; /**< Zmienna okreœlaj¹ca czy figura znajduje siê w kontenerze na figury. */
+	wxColour color; /**< Zmienna okreœlaj¹ca kolor figury. */
 };
--- Triangle.h
+++ Triangle.h
@@ -1,16 +1,51 @@
 #pragma once
 #include "Shape.h"
 
-
+/**
+* Klasa trójk¹ta reprezentuj¹ca figurê obiekt w programie. Dziedziczy po Shape.
+*/
 
 class Triangle : public Shape
 {
 public:
+	/**
+	* Konstruktor. Tworzy czworok¹t.
+	* @param x1 wspó³rzêdna x pierwszego wierzcho³ka.
+	* @param x2 wspó³rzêdna x drugiego wierzcho³ka.
+	* @param x3 wspó³rzêdna x trzeciego wierzcho³ka.
+	* @param y1 wspó³rzêdna y pierwszego wierzcho³ka.
+	* @param y2 wspó³rzêdna y drugiego wierzcho³ka.
+	* @param y3 wspó³rzêdna y trzeciego wierzcho³ka.
+	* @param c kolor figury.
+	* @param t typ figury czy trójk¹t b¹dŸ czworok¹t.
+	*/
 	Triangle(int x1, int x2, int x3, int y1, int y2, int y3, wxColour c, Type t);
+	/**
+	* Rysuje figure na ekran.
+	* @param panel miejsce gdzie wyrysowywana jest figura.
+	*/
 	void Draw(wxAutoBufferedPaintDC& panel) override;
+	/**
+	* Obraca figure o zadany k¹t wzglêdem jej œrodka.
+	* @param radian k¹t w radianach o jaki obracana jest figura.
+	*/
 	void Rotate(double radian) override;
+	/**
+	* Przesuwa figurê o dane wspó³rzêdne.
+	* @param x przesuniêcie x pikesli w lewo b¹dŸ w prawo.
+	* @param y przesuniêcie y pikesli w górê b¹dŸ w dó³.
+	*/
 	void Move(int dx, int dy) override;
+	/**
+	* Zwraca punkty w których zawartê s¹ wierzcho³ki figury.
+	*/
 	wxPoint* GetPoints() const override { return pos; }
+	/**
+	* Ustawia aktualne po³o¿enie figury na po³o¿enie pocz¹tkowe.
+	*/
 	void Reset() const override;
+	/**
+	* Destruktor.
+	*/
 	~Triangle();
 };
