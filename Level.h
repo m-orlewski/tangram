7f945f1bb1db64377d544772e56aeb2d4e61cb64
m-orlewski <morlewski@protonmail.com>
2021-06-08 15:14:18 +0200

    full documentation

--- Level.h
+++ Level.h
@@ -13,21 +13,49 @@
 #include <wx/dcclient.h>
 #include <wx/dcbuffer.h>
 
+/**
+* Struktura reprezentuj¹ca wielok¹t.
+*/
 struct Poly
 {
-	std::vector<wxPoint> points;
-	Type type;
+	std::vector<wxPoint> points; /**<Wektor zawieraj¹cy punkty bêd¹ce wierzcho³kami wielok¹ta. */
+	Type type; /**<Zmienna przechowuj¹ca informacje jakiego typu jest wielok¹t. */
+
+	/**
+	* Konstruktor tworz¹cy wielok¹t.
+	* @param Points wektor punktów bêd¹cych wierzcho³kami wielok¹ta
+	* @param T typ wielok¹ta
+	*/
 	Poly(std::vector<wxPoint> Points, Type T) : points(Points), type(T) {}
 };
 
+/**
+* Klasa reprezentuj¹ca wybrany poziom gry.
+*/
 class Level
 {
 private:
-	std::vector<wxPoint> polygon;
-	std::vector<Poly> check;
-	std::vector<Poly> check_copy;
+	std::vector<wxPoint> polygon; /**<Wektor zawieraj¹y punkty bie¿¹ce wierzcho³kami kszta³tu do u³o¿enia */
+	std::vector<Poly> check; /**<Wektor punktów u¿ywany przy sprawdzaniu kszta³tu u³o¿onego */
+	std::vector<Poly> check_copy; /**< Kopia wektora check */
 public:
+	/**
+	* Metoda, która odpowiada za tworzenie poziomu.
+	* @param filename1 œcie¿ka do pliku zawieraj¹cego wspó³rzêdne wierzcho³ków wielok¹ta do u³o¿enia
+	* @param filename2 œcie¿ka do pliku zawieraj¹cego wspó³rzêdne wierzcho³ków wielok¹ta u¿ywane do sprawdzenie poprawnoœci u³o¿enia kszta³tu.
+	*/
 	void SetLevel(std::string filename1, std::string filename2);
+
+	/**
+	* Metoda, wyrysowuj¹ca kszta³t do u³o¿enia na ekran.
+	* @param panel obiekt u¿ywany do rysowania
+	*/
 	void Draw(wxAutoBufferedPaintDC& panel);
+
+	/**
+	* Metoda sprawdzaj¹ca czy kszta³t jest u³o¿ony poprawnie.
+	* @param shapes wektor obiektów Shape uk³adanych przez u¿ytkownika
+	* @return true je¿eli u³o¿enie jest poprawne, w przeciwnym razie false
+	*/
 	bool CheckLevel(const std::vector<std::unique_ptr<Shape>>& shapes);
 };
--- vecmat.h
+++ vecmat.h
@@ -3,19 +3,46 @@
 #include <stdio.h>
 #include <iostream>
 
+/**
+* Klasa Vector reprezentuj¹ca wektor o trzech sk³adowych.
+*/
 class Vector
 {
 public:
-    double dane[3];
+    double dane[3];/**< Tablica przechowuj¹ca informacje o sk³adowych wektora. */
+
+    /**
+    * Konsturktor. Tworzy wektor [0.0, 0.0, 1.0]
+    */
     Vector();
 };
 
+/**
+* Klasa Matrix reprezentuj¹ca macierz o wymiarze 3x3.
+*/
 class Matrix
 {
 public:
-    double dane[3][3];
+    double dane[3][3]; /**< Tablica przechowuj¹ca informacje o elementach macierzy. */
+
+    /**
+    * Konstruktor. Tworzy macierz { [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0]}
+    */
     Matrix();
+
+    /**
+    * Przeci¹¿ony operator * pozwalaj¹cy na wykonanie operacji mno¿enia Matrix*Vector
+    * @param macierz macierz, przez któr¹ mno¿ymy wektor
+    * @param wektor wektor, który mno¿ymy przez macierz
+    * @return wektor bêd¹cy wynikiem naszego mno¿enia
+    */
     friend Vector operator*(const Matrix&, const Vector&);
+
+    /**
+    * Przeci¹¿ony operator * pozwalaj¹cy na wykonanie operacji mno¿enia Matrix*Matrix
+    * @param macierz macierz, przez który mno¿ymy macierz
+    * @return wynik mno¿enia
+    */
     Matrix operator*(const Matrix& macierz);
 };
 
